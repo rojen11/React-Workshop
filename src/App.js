c8fd491fb3b8e57199da20a6f0c9da203b8e6215
Rojen Maharjan <44132059+rojen11@users.noreply.github.com>
2021-10-02 15:51:38 +0545

    added state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import All from "./pages/all";
 import Completed from "./pages/completed";
 import Pending from "./pages/pending";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  function addTask(task) {
+    setTasks([...tasks, { id: nanoid(), task, completed: false }]);
+  }
+
+  function removeTask(id) {
+    setTasks(tasks.filter((t) => t.id !== id));
+  }
+
+  function completeTask(id) {
+    setTasks(
+      tasks.map((t) => (t.id === id ? { ...t, completed: !t.completed } : t))
+    );
+  }
+
+  function editTask(id, task) {
+    setTasks(tasks.map((t) => (t.id === id ? { ...t, task } : t)));
+  }
+
   return (
     <div className="bg-background h-screen w-screen">
       <Router>
